fe2a29b91a49fbccdeb8125c52ea07df1090c712
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-01-12 01:26:48 +0100

    :fire: Update the GUI.

--- main.py
+++ main.py
@@ -80,44 +80,41 @@ def extract_create(file_path, delimiter):
     create_anki_deck(qa_list)
     create_csv(qa_list)
 
+def on_select_file():
+    filepath = filedialog.askopenfilename()
+    file_path_entry.delete(0, tk.END)
+    file_path_entry.insert(0, filepath)
 
-extract_create("javed.txt", "Answer")
 
-# def on_select_file():
-#     filepath = filedialog.askopenfilename()
-#     file_path_entry.delete(0, tk.END)
-#     file_path_entry.insert(0, filepath)
-#
-#
-# def on_submit():
-#     file_path = file_path_entry.get()
-#     delimiter = delimiter_entry.get()
-#     extract_create(file_path, delimiter)
-#     result_label.config(text="Anki Deck and CSV file created!")
-#
-#
+def on_submit():
+    file_path = file_path_entry.get()
+    delimiter = delimiter_entry.get()
+    extract_create(file_path, delimiter)
+    result_label.config(text="Anki Deck and CSV file created!")
+
+
+root = tk.Tk()
+root.title("Question-Answer Extractor")
+
+# file_path_
 # root = tk.Tk()
-# root.title("Question-Answer Extractor")
-#
-# # file_path_
-# # root = tk.Tk()
-#
-# # create the widgets
-# file_path_label = tk.Label(root, text="File path:")
-# file_path_entry = tk.Entry(root)
-# select_file_button = tk.Button(root, text="Select file", command=on_select_file)
-# delimiter_label = tk.Label(root, text="Delimiter:")
-# delimiter_entry = tk.Entry(root)
-# submit_button = tk.Button(root, text="Submit", command=on_submit)
-# result_label = tk.Label(root)
-#
-# # arrange the widgets
-# file_path_label.grid(row=0, column=0, padx=5, pady=5)
-# file_path_entry.grid(row=0, column=1, padx=5, pady=5)
-# select_file_button.grid(row=0, column=2, padx=5, pady=5)
-# delimiter_label.grid(row=1, column=0, padx=5, pady=5)
-# delimiter_entry.grid(row=1, column=1, padx=5, pady=5)
-# submit_button.grid(row=1, column=2, padx=5, pady=5)
-# result_label.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
-#
-# root.mainloop()
+
+# create the widgets
+file_path_label = tk.Label(root, text="File path:")
+file_path_entry = tk.Entry(root)
+select_file_button = tk.Button(root, text="Select file", command=on_select_file)
+delimiter_label = tk.Label(root, text="Delimiter:")
+delimiter_entry = tk.Entry(root)
+submit_button = tk.Button(root, text="Submit", command=on_submit)
+result_label = tk.Label(root)
+
+# arrange the widgets
+file_path_label.grid(row=0, column=0, padx=5, pady=5)
+file_path_entry.grid(row=0, column=1, padx=5, pady=5)
+select_file_button.grid(row=0, column=2, padx=5, pady=5)
+delimiter_label.grid(row=1, column=0, padx=5, pady=5)
+delimiter_entry.grid(row=1, column=1, padx=5, pady=5)
+submit_button.grid(row=1, column=2, padx=5, pady=5)
+result_label.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
+
+root.mainloop()
